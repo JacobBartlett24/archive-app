5f3e55541e7ca3ef30140fdd4a3f7cd3453b3fb9
JacobBartlett24 <mbart6869@gmail.com>
2022-11-03 22:03:29 -0500

    Add state to images

--- src/ViewPlaylist.js
+++ src/ViewPlaylist.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Image from "./Images/PngItem_851857.png";
+import {ReactComponent as ArrowIcon} from './Images/arrow-right-solid.svg'
 
 function ViewPlaylist(){
 
-    const [currentImage,switchImage] = useState(Image);
+    const [currentImage,switchImage] = useState(0);
     
     const populateImageSlider= () =>{
 
@@ -17,23 +18,34 @@ function ViewPlaylist(){
         console.log(imagePlaylist)
     }
 
-    populateImageSlider();
+    const nextImage = (e) =>{
+
+        if(e.currentTarget.id === 'previous' && currentImage != 0){    
+            switchImage(currentImage - 1);
+        } else if (e.currentTarget.id === 'next'){
+            switchImage(currentImage + 1);
+        }
+
+        
+    }
     
+    populateImageSlider();
 
     return(
         <div>
             View Playlist Page
             
             <div id="imageSlider">
-                <div id="previousImage">
-                    <img src="" alt="" />
+                <div id="previousArrow">
+                    <a onClick={nextImage} id="previous"><ArrowIcon /></a>
                 </div>
                 <div id="currentImage">
-                    <img src="" alt="" />
+                    <img className="image" src={Image} alt="" />
                 </div>
-                <div id="nextImage">
-                    <img src="" alt="" />
+                <div id="nextArrow">
+                    <a id="next" onClick={nextImage}><ArrowIcon /></a>
                 </div>
+                <div>{currentImage}</div>
             </div>
         </div>
     );

3863eb27c0835647963b9b4210aae677f886ce69
JacobBartlett24 <mbart6869@gmail.com>
2022-11-04 16:25:12 -0500

    Add imageslider functionality

--- src/ViewPlaylist.js
+++ src/ViewPlaylist.js
@@ -1,22 +1,33 @@
 import { useState } from "react";
-import Image from "./Images/PngItem_851857.png";
+import imageSrc from "./Images/PngItem_851857.png";
 import {ReactComponent as ArrowIcon} from './Images/arrow-right-solid.svg'
+
+class Image{
+    constructor(src,id){
+        this.src = src
+        this.id = id
+    }
+}
+
 
 function ViewPlaylist(){
 
     const [currentImage,switchImage] = useState(0);
-    
+
     const populateImageSlider= () =>{
 
         let imagePlaylist = []
 
         for (let i = 0; i < 10; i++){
-            imagePlaylist.push(Image)
-            console.log(Image)
+            let image = new Image;
+            image.id = i;
+            image.src = imageSrc; 
+            imagePlaylist.push(image)        
         }
-
-        console.log(imagePlaylist)
+        return imagePlaylist;
     }
+
+    let imagePlaylist = populateImageSlider();
 
     const nextImage = (e) =>{
 
@@ -25,11 +36,11 @@ function ViewPlaylist(){
         } else if (e.currentTarget.id === 'next'){
             switchImage(currentImage + 1);
         }
-
         
+        console.log()
     }
     
-    populateImageSlider();
+    
 
     return(
         <div>
@@ -40,7 +51,9 @@ function ViewPlaylist(){
                     <a onClick={nextImage} id="previous"><ArrowIcon /></a>
                 </div>
                 <div id="currentImage">
-                    <img className="image" src={Image} alt="" />
+                    <img className="image"
+                         id={imagePlaylist[currentImage].id}
+                         src={imagePlaylist[currentImage].src} alt="" />
                 </div>
                 <div id="nextArrow">
                     <a id="next" onClick={nextImage}><ArrowIcon /></a>
